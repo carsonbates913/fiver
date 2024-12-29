07888340ec63053842775a7623b2309a3065115a
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-28 19:00:58 -0500

    -feat: add navigation components

---
+++ frontend/src/components/Navigation/MainNavigation.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+import './MainNavigation.css';
+import SideDrawer from './SideDrawer.jsx';
+import NavLinks from './NavLinks.jsx';
+import Backdrop from '../UIElements/Backdrop.jsx';
+
+export default function MainNavigation(props) {
+  return (
+    <>
+      {!props.isStatic && (
+        <Backdrop show={props.isNavVisible} onClick={props.toggleNav} />
+      )}
+      <SideDrawer show={props.isNavVisible} isStatic={props.isStatic}>
+        <div className="main-navigation__logo">
+          <NavLink>
+            <img></img>
+          </NavLink>
+        </div>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+    </>
+  )
+}
+
+MainNavigation.propTypes = {
+  isNavVisible: PropTypes.bool.isRequired,
+  toggleNav: PropTypes.func,
+  isStatic: PropTypes.bool,
+}
---
+++ frontend/src/components/Navigation/SideDrawer.jsx
@@ -0,0 +1,34 @@
+import ReactDOM from 'react-dom'
+import { CSSTransition } from 'react-transition-group';
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import './SideDrawer.css'
+
+export default function SideDrawer(props) {
+
+  const nodeRef = useRef(null);
+
+  const content =   
+    <CSSTransition nodeRef={nodeRef} in={props.show} timeout={200} classNames="slide-in-left" mountOnEnter unmountOnExit>
+      <aside className="side-drawer" ref={nodeRef}>
+        {props.children}
+      </aside>
+    </CSSTransition>
+
+  if(props.isStatic){
+    return (
+      <aside className="side-drawer" ref={nodeRef} style={{position: "relative", display: "inline-flex"}}>
+        {props.children}
+      </aside>
+    )
+  }else {
+    return ReactDOM.createPortal(content, document.getElementById('nav-portal-root'));
+  }
+}
+
+SideDrawer.propTypes = {
+  show: PropTypes.bool.isRequired,
+  children: PropTypes.element,
+  isStatic: PropTypes.bool.isRequired,
+}
