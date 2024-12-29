e978c840cf471d33cd7947e34590d9f24361d430
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-29 15:03:33 -0500

    -feat: create QuoteItem and QuoteList components

--- frontend/src/components/Profile/QuotesList.jsx
+++ frontend/src/components/Profile/QuotesList.jsx
@@ -1,7 +1,20 @@
-import './QuotesList.css'
+import PropTypes from 'prop-types';
 
-export default function QuotesList() {
+import './QuotesList.css'
+import QuoteItem from './QuoteItem.jsx';
+
+export default function QuotesList(props) {
   return (
-    <></>
+    <>
+      <ul className="quotes-list">
+        <QuoteItem prompt={"Fun Fact?"} quote={props.funFact}/>
+        <QuoteItem prompt={"Favorite Dartmouth Tradition?"} quote={props.favoriteDartmouthTradition}/>
+      </ul>
+    </>
   )
+}
+
+QuotesList.propTypes = {
+  funFact: PropTypes.string.isRequired,
+  favoriteDartmouthTradition: PropTypes.string.isRequired,
 }
