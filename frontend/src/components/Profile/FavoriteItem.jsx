e82a2fbe2a81be6ab7507be19440158acf293de5
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-29 15:03:06 -0500

    -feat: create favoriteItem and favoritesList components

--- frontend/src/components/Profile/FavoriteItem.jsx
+++ frontend/src/components/Profile/FavoriteItem.jsx
@@ -1,7 +1,18 @@
+import PropTypes from 'prop-types';
+
 import './FavoriteItem.css'
 
-export default function FavoriteItem() {
+export default function FavoriteItem(props) {
   return (
-    <></>
+    <>
+    <li className="favorite-item">
+      <div className="favorite-item__number-card">1</div>
+      <h3>{props.thing}</h3>
+    </li>
+    </>
   )
+}
+
+FavoriteItem.propTypes = {
+  thing: PropTypes.string.isRequired,
 }
--- frontend/src/components/Profile/FavoritesList.jsx
+++ frontend/src/components/Profile/FavoritesList.jsx
@@ -1,7 +1,22 @@
-import './FavoritesList.css'
+import PropTypes from 'prop-types';
 
-export default function FavoritesList() {
+import './FavoritesList.css'
+import FavoriteItem from './FavoriteItem.jsx';
+
+export default function FavoritesList(props) {
   return (
-    <></>
+    <>
+      <ul className="favorites-list">
+        <FavoriteItem thing={props.favoriteThing1}/>
+        <FavoriteItem thing={props.favoriteThing2}/>
+        <FavoriteItem thing={props.favoriteThing3}/>
+      </ul>
+    </>
   )
+}
+
+FavoritesList.propTypes = {
+  favoriteThing1: PropTypes.string.isRequired,
+  favoriteThing2: PropTypes.string.isRequired,
+  favoriteThing3: PropTypes.string.isRequired,
 }
