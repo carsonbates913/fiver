bd6969c68bfe6e8387464fd5af0fed105c380294
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-31 10:04:11 -0500

    -feat: add feed and five components

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,7 +1,20 @@
+import PropTypes from 'prop-types';
+
 import './Feed.css';
 
-export default function Feed() {
+export default function Feed(props) {
   return (
-    <></>
+    <div className= "feed">
+      <header className="feed__header">
+        <p>12/9</p>
+      </header>
+      <div className="feed__content">
+        {props.children}
+      </div>
+    </div>
   )
+}
+
+Feed.propTypes = {
+  children: PropTypes.node,
 }
