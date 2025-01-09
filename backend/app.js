43f12a3ebd26523f0063ba3b3870eabda74681a9
Carson Bates <carson.d.bates.27@dartmouth.edu>
2025-01-09 17:57:33 -0500

    -feat: add MongoDB connection string and error handling

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const HttpError = require('./models/error.js');
+const HttpError = require('./models/error');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 const fivesRoutes = require('./routes/fives-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -8,6 +11,13 @@ const usersRoutes = require('./routes/users-routes');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+})
 
 app.use('/api/fives', fivesRoutes); 
 
@@ -17,13 +27,19 @@ app.use(( req, res, next )  => {
   throw new HttpError("Could not find this route", 404);
 })
 
-
 app.use((error, req, res, next) => {
-  if(res.headerSent){
+  if (res.headerSent) {
     return next(error);
   }
-  
-  return res.status(error.code || 500).json({message: error.message || "an unknown error ocurred"});
-})
 
-app.listen(3000);
+  console.log(error.message);
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
+});
+
+mongoose.connect(process.env.MONGO_PASSWORD)
+  .then(() =>
+    app.listen(3000))
+  .catch((error) => {
+    console.log(error);
+  });
