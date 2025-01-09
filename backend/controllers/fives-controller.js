f317aafc446ef1b8caed3bbf6772204f1a4ab214
Carson Bates <carson.d.bates.27@dartmouth.edu>
2025-01-09 17:59:11 -0500

    feat: incorporate MongoDB into CRUD operations

--- backend/controllers/fives-controller.js
+++ backend/controllers/fives-controller.js
@@ -1,89 +1,144 @@
-const HttpError = require('../models/error.js');
+const HttpError = require('../models/error');
 const { validationResult } = require('express-validator');
-const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Five = require('../models/five');
+const User = require('../models/user');
 
-let DUMMY_FIVES = [
-  {
-    id: 'f1',
-    to: 'Carson',
-    toId: 'u1',
-    words: ["amazing", "smart"],
-    message: "ur great!",
-    from: "Akash",
-    fromId: 's1',
-  }
-]
-
-
-const getFiveById = (req, res, next) => {
+const getFiveById = async (req, res, next) => {
   const fiveId = req.params.fid;
-  const five = DUMMY_FIVES.find((five) => five.id===fiveId);
-  console.log(five);
+
+  let five;
+  try {
+    five = await Five.findById(fiveId);
+  } catch(error) {
+    return next(new HttpError("Something went wrong, could not find a five"), 400);
+  }
 
   if(!five){
     return next(new HttpError("Could not find a five for the provided id", 400));
   }
 
-  res.json(five);
+  res.json({five: five.toObject({getters: true})});
 }
 
-const getFivesByUserId = (req, res, next) => {
+const getFivesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const fives = DUMMY_FIVES.filter((five) => five.toId===userId);
+
+  let fives;
+  try {
+    fives = await Five.find({sender: userId});
+  } catch(error) {
+    next(new HttpError("Something went wrong, could not find fives"));
+  }
 
   if(!fives || fives.length===0){
     return next(new HttpError("Could not find fives for the provided user id"));
   }
 
-  res.status(200).json(fives); 
+  res.status(200).json({five: fives.map((five) => five.toObject({getters: true}))}); 
 }
 
-const createFive = (req, res, next) => {
+const createFive = async (req, res, next) => {
   const errors = validationResult(req)
   if(!errors.isEmpty()){
-    throw new HttpError("Invalid inputs", 422);
+    return next(new HttpError("Invalid inputs", 422));
   }
 
-  const { to, toId, words, message, from, fromId } = req.body;
-  const createdFive = {
-    id: uuid.v4(),
-    to,
-    toId,
-    words,
-    message,
-    from,
-    fromId,
+  const { to, toId, words, message, from, sender } = req.body;
+  
+  const createdFive = new Five(
+    {
+      to,
+      toId,
+      words,
+      message,
+      from,
+      sender,
+    }
+  )
+
+  let user;
+  try {
+    user = await User.findById(sender);
+  } catch (error) {
+    return next(new HttpError("Something went wrong, could not find sender"));
   }
 
-  DUMMY_FIVES.push(createdFive);
+  if (!user) {
+    const error = new HttpError('Could not find user for provided id', 404);
+    return next(error);
+  }
+
+  console.log(user);
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await createdFive.save({session: sess});
+    user.fives.push(createdFive);
+    await user.save({session: sess});
+    await sess.commitTransaction();
+  } catch (error) {
+    return next(new HttpError("Somethign went wrong, could not save five"));
+  }
 
   res.status(201).json(createdFive);
 }
 
-const updateFive = (req, res, next) => {
+const updateFive = async (req, res, next) => {
   const errors = validationResult(req)
   if(!errors.isEmpty()){
-    throw new HttpError("Invalid inputs", 422);
+    return next(new HttpError("Invalid inputs", 422));
   }
 
   const { words, message } = req.body;
   const fiveId = req.params.fid;
 
-  const updatedFive = {...DUMMY_FIVES.find((five) => five.id === fiveId)};
-  const fiveIndex = DUMMY_FIVES.findIndex((five) => five.id === fiveId);
+  let updatedFive;
+  try {
+    updatedFive =  await Five.findById(fiveId);
+  } catch(error) {
+    return next(new HttpError("Something went wrong, could not updated fives"));
+  }
 
   updatedFive.words = words;
-  updatedFive.message = message; 
+  updatedFive.message = message;
 
-  DUMMY_FIVES[fiveIndex] = updatedFive;
+  try {
+    await updatedFive.save();
+  } catch(error) {
+    return next(new HttpError("Something went wrong, could not save updated five"));
+  }
 
-  res.status(200).json(updatedFive);
+  res.status(200).json({updatedFive: updatedFive.toObject({getters: true})});
 }
 
-const deleteFive = (req, res, next) => {
+const deleteFive = async (req, res, next) => {
   const fiveId = req.params.fid;
-  
-  DUMMY_FIVES = DUMMY_FIVES.filter((five) => five.id !== fiveId);
+
+  let five;
+  try {
+    five = await Five.findById(fiveId).populate('sender');
+  } catch(error) {
+    return next(new HttpError("Something went wrong, could not find five to be deleted"));
+  }
+
+  if(!five){
+    return next(new HttpError("Could not find a five with the provided id"));
+  }
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await Five.findByIdAndDelete(fiveId).session(sess);
+    five.sender.fives.pull(five);
+    await five.sender.save({session: sess});
+    await sess.commitTransaction();
+  } catch (error) {
+    return next(new HttpError("Something went wrong, could not find user attached to fiver to be deleted"));
+  }
+
+
 
   res.status(200).json("Deleted five");
 }
