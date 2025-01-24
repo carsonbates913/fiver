46e36ab6bf6abec3dd75c943549bc21d673d56c5
Carson Bates <carson.d.bates.27@dartmouth.edu>
2025-01-24 09:14:32 -0500

    -feat: transfer authContext state management to app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider, Outlet, useLocation, matchPath } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 import FeedPage from './pages/FeedPage/FeedPage.jsx';
 import LoginPage from './pages/LoginPage/LoginPage.jsx';
@@ -7,7 +7,7 @@ import TeamPage from './pages/TeamPage/TeamPage.jsx';
 import ProfilePage from './pages/ProfilePage/ProfilePage.jsx';
 import MainNavigation from './components/Navigation/MainNavigation.jsx';
 import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
-import AuthProvider from './context/AuthContext.jsx';
+import { AuthContext } from './context/AuthContext.jsx';
 import ProtectedRoute from './routes/ProtectedRoute.jsx';
 
 function RootLayout() {
@@ -39,6 +39,35 @@ function RootLayout() {
 
 function App() {
 
+  const [token, setToken] = useState();
+  const [userId, setUserId] = useState();
+  const [name, setName] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const login = useCallback((name, userId, token, expirationDate) => {
+    setToken(token);
+    setUserId(userId);
+    setName(name);
+    const tokenExpiration = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    localStorage.setItem("fiverUserData", JSON.stringify({name: name, userId: userId, token: token, expiration: tokenExpiration.toISOString()}));
+  }, [])
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserId(null);
+    setName(null);
+    localStorage.removeItem('fiverUserData');
+  }, [])
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem('fiverUserData'));
+    if(userData && userData.token && new Date(userData.expiration) > new Date()){
+      login(userData.name, userData.userId, userData.token, new Date(userData.expiration));
+      console.log("logged in already");
+    }
+    setLoading(false);
+  }, [login])
+
   const router = createBrowserRouter([
     {
       path: '/',
@@ -66,12 +95,16 @@ function App() {
       }]
     },
   ]);
+
+  if(loading){
+    return <>loading</>
+  }
  
   return (
     <>
-      <AuthProvider>
+      <AuthContext.Provider value={{isLoggedIn: !!token, token, logout, login, name, userId}}>
         <RouterProvider router={router} />
-      </AuthProvider>
+      </AuthContext.Provider>
     </>
   )
 }
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,33 +1,10 @@
-import { createContext, useState, useCallback } from 'react';
-import PropTypes from 'prop-types';
+import { createContext} from 'react';
 
-export const AuthContext = createContext({});
-
-export default function AuthProvider(props) {
-
-  const [token, setToken] = useState();
-  const [userId, setUserId] = useState();
-  const [name, setName] = useState();
-
-  const login = useCallback((name, user, token) => {
-    setToken(token);
-    setUserId(user);
-    setName(name);
-  }, [])
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setUserId(null);
-    setName(null);
-  }, [])
-
-  return (
-    <AuthContext.Provider value={{isLoggedIn: !!token, token, logout, login, name, userId}}>
-      {props.children}
-    </AuthContext.Provider>
-  )
-}
-
-AuthProvider.propTypes = {
-  children: PropTypes.node,
-}
+export const AuthContext = createContext({
+  isLoggedIn: false,
+  userId: null,
+  token: null,
+  name: null,
+  login: () => {},
+  logout: () => {}
+});
