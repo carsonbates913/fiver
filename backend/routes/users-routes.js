b34de82379009aa8287b441592a291290c7f76eb
Carson Bates <carson.d.bates.27@dartmouth.edu>
2025-01-27 05:04:00 -0500

    -feat: add edit profile route

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check } = require('express-validator');
 const usersController = require('../controllers/users-controller');
 const checkAuth = require('../middleware/auth');
+const fileUpload = require('../middleware/file-upload'); 
 
 const router = express.Router();
 
@@ -18,5 +19,26 @@ router.use(checkAuth);
 router.get('/', usersController.getUsers);
 
 router.get('/profile/:uid', usersController.getUserById);
+
+router.patch('/profile/:uid', 
+  fileUpload.single('picture'),
+  [
+  check('year').not().isEmpty(),
+  check('dev').not().isEmpty(),
+  check('des').not().isEmpty(),
+  check('pm').not().isEmpty(),
+  check('core').not().isEmpty(),
+  check('mentor').not().isEmpty(),
+  check('major').not().isEmpty(),
+  check('minor').not().isEmpty(),
+  check('birthday').not().isEmpty(),
+  check('home').not().isEmpty(),
+  check('quote').not().isEmpty(),
+  check('favoriteThing1').not().isEmpty(),
+  check('favoriteThing2').not().isEmpty(),
+  check('favoriteThing3').not().isEmpty(),
+  check('favoriteDartmouthTradition').not().isEmpty(),
+  check('funFact').not().isEmpty(),
+], usersController.updateUser);
 
 module.exports = router;
